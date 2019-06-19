9e4c7d6fc78cc8cbb4ab8cf494f9a1b44f435045
Sankalp100 <sankalpgour123@gmail.com>
2019-06-19 21:00:58 +0530

    routing

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ var app = angular.module('app',['ngRoute']);
 app.config(['$routeProvider', function($routeProvider){
   $routeProvider.when('/plan', {
     // template: '<strong>Plan<strong>'
-    templateUrl: 'Plan/index.html'
+    templateUrl: 'Plan/plan.html',
+    controller: 'Plan/plan.js'
   })
+  .when('/dietitian', {
+    templateUrl: 'Dietitian/dietitian.html',
+    controller: 'Dietitian/dietitian.html'
+  })
+
 }]);
+
+app.controller('message', ['$scope', function($scope) {
+  $scope.a=10;
+  $scope.b=20;
+
+}])
