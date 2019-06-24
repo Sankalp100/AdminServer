7fbd583dde3b1c6562b825efe0f2a1273456c7c2
Sankalp100 <sankalpgour123@gmail.com>
2019-06-24 12:44:54 +0530

    uom bug

--- Uom/uom.js
+++ Uom/uom.js
@@ -115,57 +115,7 @@ myApp.controller('studentController', ['$scope', '$http', function($scope, $http
     $scope.userid = true;
   };
 
-  //////////////////Shortcut methods for $http//////
-
-  //$http GET
-  //$http.get('https://jsonplaceholder.typicode.com/users', function successCallback(response) {
-  //
-  //  alert("User has updated Successfully")
-  //
-  //}, function errorCallback(response) {
-  //
-  //  alert("Error. while updating user Try Again!");
-  //
-  //});
-
-
-  //$http POST
-  //$http.post('https://jsonplaceholder.typicode.com/users', $scope.user, function successCallback(response) {
-  //
-  //  $scope.users.push(response.data);
-  //alert("User has created Successfully")
-  //
-  //}, function errorCallback(response) {
-  //
-  //  alert("Error. while created user Try Again!");
-  //
-  //});
-
-
-  //$http PUT
-  //$htp.put('https://jsonplaceholder.typicode.com/users/' + $scope.user.id, $scope.user, function successCallback(response) {
-  //
-  //  alert("User has updated Successfully")
-  //
-  //}, function errorCallback(response) {
-  //
-  //  alert("Error. while updating user Try Again!");
-  //
-  //});
-
-
-  //$http DELETE
-  //$http.delete('https://jsonplaceholder.typicode.com/users/' + user.id, function successCallback(response) {
-  //
-  //  alert("User has deleted Successfully");
-  //var index = $scope.users.indexOf(user);
-  //$scope.users.splice(index, 1);
-  //
-  //  }, function errorCallback(response) {
-  //
-  //  alert("Error. while deleting user Try Again!");
-  //
-  //  });
+  
 
 
 }]);
